ee5f2ff6efa4d6be440a100f6cf897af60984989
JHW <ruby07025@gmail.com>
2024-12-13 16:07:12 +0900

    ddd

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const bp = require("body-parser");
+const cors = require('cors');
 const nunjucks = require("nunjucks");
 const mainrouter = require("./routes/mainrouter");
 const userrouter = require("./routes/userrouter");
 // 프로필 화면
 const session = require("express-session");
 const path = require("path");
+const coverrouter = require("./routes/coverrouter");//덮개
+const w_setrouter = require("./routes/w_setrouter");//수위설정
+const waterrouter = require("./routes/waterrouter");//급수
+const wrouter = require("./routes/wrouter");        //물상태
 
 // 미들웨어 설정
 app.use(express.json());
@@ -17,6 +22,10 @@ app.use(
         saveUninitialized: true,
     })
 );
+app.use("/cover",coverrouter); //덮개
+app.use("/w_set",w_setrouter); //수위설정
+app.use("/water",waterrouter); //급수
+app.use("/w",wrouter);         //물상태
 
 // 정적 파일 서빙
 app.use(express.static(path.join(__dirname, "views")));
